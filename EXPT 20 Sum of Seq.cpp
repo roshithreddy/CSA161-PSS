ba8feae34ec286cfc871b98fab215a62e61366cb
roshithreddy <113961499+roshithreddy@users.noreply.github.com>
2023-05-18 12:16:47 +0530

    Add files via upload

---
+++ EXPT 20 Sum of Seq.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n,sum=0;
+printf("\nenter n:");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+if(i%2==0)
+{
+printf("%d",i);
+sum=sum+I;
+}
+}
+printf(“the sum of even number is %d”,sum);
+return 0;
+}
---
+++ Largest and Smallest Elements of an Array.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int N[] = {5,2,36,18,25,30};
+int min = N[0];
+int max = n([0]);
+for (int i = 1;i<n;i++)
+{
+	if(N[i]< min)
+	{
+		min = N[i];
+		}
+		if(N[i]>max)
+		{
+			max = N[i];
+			}
+}
+printf("The smallest number is %d\n", min);
+printf("The largest number is %d\n", max);
+return 0;
+}
